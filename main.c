64e1b0e0e61c956aaaaf4a24f6ede7a221bdaa9e
Ahmed Ait Elaaoud <aait-ela@c1r5p3.1337.ma>
2025-11-21 18:44:44 +0100

    add main function and test file for get_next_line

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int fd;
+    char *line;
+
+    fd = open("test.txt", O_RDONLY);
+    if (fd < 0)
+    {
+        perror("Error opening file");
+        return (1);
+    }
+
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s", line);
+        free(line);
+    }
+
+    close(fd);
+    return 0;
+}
+
